f32b4de1eeb75779f438743cc85db6477fdf894c
volesh <volesh2@gmail.com>
2022-09-28 01:15:56 +0300

    lala

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,23 @@
 import {NavLink} from "react-router-dom";
 
 import {urls} from "../../services";
+import css from './header.module.css'
 
 const Header = () => {
     return (
-        <div>
-            <img src={urls.pizzaLogo} alt="Logo"/>
-            <NavLink to={'/'}>Піца</NavLink>
-            <NavLink to={'/drinks'}>Напої</NavLink>
-            <NavLink to={'/sides'}>Сайди</NavLink>
-            <NavLink to={'/deserts'}>Десерти</NavLink>
-            <NavLink to={'/checkout'}>Кошик</NavLink>
+        <div className={css.header}>
+            <div className={css.maineDiv}>
+                <div className={css.imgBlock}>
+                    <img className={css.img} src={urls.pizzaLogo} alt="Logo"/>
+                </div>
+                <div className={css.navigationBlock}>
+                    <NavLink className={css.nav} to={'/'}>Піца</NavLink>
+                    <NavLink className={css.nav} to={'/drinks'}>Напої</NavLink>
+                    <NavLink className={css.nav} to={'/sides'}>Сайди</NavLink>
+                    <NavLink className={css.nav} to={'/deserts'}>Десерти</NavLink>
+                    <NavLink className={css.nav} to={'/checkout'}>Кошик</NavLink>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/pizza/Pizza.js
+++ src/components/pizza/Pizza.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import css from './pizza.module.css'
 
 const Pizza = ({pizza}) => {
+    let [choose1, setChoose1] = useState(1)
+    let [choose2, setChoose2] = useState(1)
+    let [price, setPrice] = useState(Number(pizza.price))
+
+    const clickChooser1 = (n) =>{
+        setChoose1(n)
+    }
+    const clickChooser2 = (n) =>{
+        setChoose2(n)
+    }
+    useEffect(()=>{
+        if(choose1 === 1 && choose2 === 3){
+            setPrice(Number(pizza.price) + 44)
+        }else if(choose1 === 1 && choose2 === 4){
+            setPrice(Number(pizza.price) + 53)
+        }else if((choose1 === 2 && choose2 === 1) || (choose1 === 2 && choose2 === 2)){
+            setPrice(Number(pizza.price) + 40)
+        }else if(choose1 === 2 && choose2 === 3){
+            setPrice(Number(pizza.price) + 93)
+        }else if(choose1 === 2 && choose2 === 4){
+            setPrice(Number(pizza.price) + 100)
+        }else if((choose1 === 3 && choose2 === 1) || (choose1 === 3 && choose2 === 2) || (choose1 === 3 && choose2 ===3) || (choose1 === 3 && choose2 === 4)){
+            if(choose2 === 3 || choose2 === 4){
+                setChoose2(1)
+                setPrice(Number(pizza.price) + 70)
+            }
+            setPrice(Number(pizza.price) + 70)
+
+        }else {
+            setPrice(Number(pizza.price))
+        }
+    },[choose1,choose2])
+
     return (
         <div className={css.card}>
 
@@ -18,20 +51,20 @@ const Pizza = ({pizza}) => {
             </div>
 
             <div className={css.chooser}>
-                <span className={css.choose1}>Cтандартна</span>
-                <span className={css.choose1}>Велика</span>
-                <span className={css.choose1}>XXl</span>
+                <span className={choose1 !== 1?css.choose1:css.choosed1} onClick={()=>clickChooser1(1)}>Cтандартна</span>
+                <span className={choose1 !== 2?css.choose1:css.choosed1} onClick={()=>clickChooser1(2)}>Велика</span>
+                <span className={choose1 !== 3?css.choose1:css.choosed1} onClick={()=>clickChooser1(3)}>XXl</span>
             </div>
 
             <div className={css.chooser}>
-                <span className={css.choose2}>Стандартне</span>
-                <span className={css.choose2}>Тонке</span>
-                <span className={css.choose2}>Філадельфія</span>
-                <span className={css.choose2}>Борт Хот-Дог</span>
+                <button  className={choose2 !== 1?css.choose2:css.choosed2} onClick={()=>clickChooser2(1)}>Стандартне</button>
+                <button className={choose2 !== 2?css.choose2:css.choosed2} onClick={()=>clickChooser2(2)}>Тонке</button>
+                <button disabled={choose1===3} className={choose2 !== 3?css.choose2:css.choosed2} onClick={()=>clickChooser2(3)}>Філадельфія</button>
+                <button disabled={choose1===3} className={choose2 !== 4?css.choose2:css.choosed2} onClick={()=>clickChooser2(4)}>Борт Хот-Дог</button>
             </div>
 
             <div className={css.lastDiv}>
-                <p>{pizza.price} грн</p>
+                <p>{price} грн</p>
                 <button>В кошик</button>
             </div>
 
