0a193950466e5f55fdb4c1c71cae576a93a17eea
volesh <volesh2@gmail.com>
2022-09-28 20:07:12 +0300

    lala

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import {Routes, Route} from "react-router-dom";
 import {PizzaPage} from "./pages";
-import {Header} from "./components/header/Header";
+import {Header} from "./components";
+
+import {useEffect, useState} from "react";
+import React from "react";
 
 
 function App() {
   return (
+
     <div>
         {<Header/>}
       <Routes>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,23 @@
 import {NavLink} from "react-router-dom";
+import {useState, useEffect} from "react";
 
 import {urls} from "../../services";
 import css from './header.module.css'
 
 const Header = () => {
+    const [scroll, setScroll] = useState(0);
+
+    const handleScroll = () => {
+        setScroll(window.scrollY);
+    };
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
         <div className={css.header}>
-            <div className={css.maineDiv}>
+            <div className={scroll === 0?css.maineDiv:css.maineDivScroll}>
                 <div className={css.imgBlock}>
                     <img className={css.img} src={urls.pizzaLogo} alt="Logo"/>
                 </div>
