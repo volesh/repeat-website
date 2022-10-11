e9534a8457f4ef77698d0d36faea783c9e21df52
volesh <volesh2@gmail.com>
2022-10-11 17:43:08 +0300

    lala

---
+++ src/assets/pizzas.js
@@ -0,0 +1,97 @@
+pizzas = [
+    {
+      name: 'Мангеттен',
+      image: 'https://media.dominos.ua/__sized__/menu/product_osg_image_category/2021/07/27/Manhatten_slice_collageweb-min-thumbnail-960x960-70.jpg',
+      ingredients: ['(подвійна порція грибів)', 'Гриби', 'Моцарела', 'Пепероні', 'Соус Альфредо'],
+      sizes: ['26', '30', '40'],
+      prices: {'26':'220', '30':'260', '40':'290'},
+      weights: {'26':'570', '30':'765', '40':'847'}
+    },
+
+    {
+        name: 'Пепероні з томатами',
+        image: 'https://media.dominos.ua/__sized__/menu/product_osg_image_mobile/2018/02/28/%D0%9F%D0%B5%D0%BF%D0%BF%D0%B5%D1%80%D0%BE%D0%BD%D0%B8_%D0%B8_%D1%82%D0%BE%D0%BC%D0%B0%D1%82%D1%8B_300dpi-thumbnail-960x960-70.jpg',
+        ingredients: ['Моцарела', 'Пепероні', 'Помідори', 'Соус Барбекю'],
+        sizes: ['26', '30', '40'],
+        prices: {'26':'220', '30':'260', '40':'290'},
+        weights: {'26':'554', '30':'738', '40':'787'}
+    },
+
+    {
+        name: 'Шинка та гриби',
+        image: 'https://media.dominos.ua/__sized__/menu/product_osg_image_category/2020/12/28/Vetchina_i_griby_-thumbnail-960x960-70.jpg',
+        ingredients: ['Шинка', 'Гриби', 'Моцарела', 'Соус Авторський'],
+        sizes: ['26', '30', '40'],
+        prices: {'26':'220', '30':'260', '40':'290'},
+        weights: {'26':'545', '30':'764', '40':'826'}
+    },
+
+    {
+        name: 'Техас',
+        image: 'https://media.dominos.ua/__sized__/menu/product_osg_image_category/2019/10/03/%D0%A2%D0%B5%D1%85%D0%B0%D1%81_300dpi-min-thumbnail-960x960-70.jpg',
+        ingredients: ['Кукурудза', 'Цибуля', 'Гриби', 'Ковбаски баварські', 'Моцарела', 'Соус Барбекю'],
+        sizes: ['26', '30', '40'],
+        prices: {'26':'220', '30':'260', '40':'290'},
+        weights: {'26':'550', '30':'738', '40':'807'}
+    },
+
+    {
+        name: 'Маргарита',
+        image: 'https://media.dominos.ua/__sized__/menu/product_osg_image_category/2019/10/04/%D0%9C%D0%B0%D1%80%D0%B3%D0%B0%D1%80%D0%B8%D1%82%D0%B0_300dpi-min-thumbnail-960x960-70.jpg',
+        ingredients: ['(подвійна порція моцарели)', 'Моцарела', 'Соус Авторський'],
+        sizes: ['26', '30', '40'],
+        prices: {'26':'255', '30':'302', '40':'339'},
+        weights: {'26':'552', '30':'800', '40':'831'}
+    },
+
+    {
+        name: 'Барбекю',
+        image: 'https://media.dominos.ua/__sized__/menu/product_osg_image_mobile/2018/02/22/BBQ-thumbnail-960x960-70.jpg',
+        ingredients: ['Курка', 'Цибуля', 'Бекон', 'Гриби', 'Моцарела', 'Соус Барбекю'],
+        sizes: ['26', '30', '40'],
+        prices: {'26':'260', '30':'307', '40':'344'},
+        weights: {'26':'560', '30':'760', '40':'800'}
+    },
+
+    {
+        name: 'Карбонара',
+        image: 'https://media.dominos.ua/__sized__/menu/product_osg_image_category/2019/10/04/%D0%9A%D0%B0%D1%80%D0%B1%D0%BE%D0%BD%D0%B0%D1%80%D0%B0_300dpi-min-thumbnail-960x960-70.jpg',
+        ingredients: [],
+        sizes: [26, 30, 40],
+        prices: {},
+        weights: {},
+        category: []
+    },
+
+    {
+        name: '',
+        image: '',
+        ingredients: [],
+        sizes: [26, 30, 40],
+        prices: {},
+        weights: {},
+        category: []
+    },
+
+    {
+        name: '',
+        image: '',
+        ingredients: [],
+        sizes: [26, 30, 40],
+        prices: {},
+        weights: {},
+        category: []
+    },
+
+    {
+        name: '',
+        image: '',
+        ingredients: [],
+        sizes: [26, 30, 40],
+        prices: {},
+        weights: {},
+        category: []
+    },
+
+
+]
